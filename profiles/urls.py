b04420de8083fb1625e0a3cb1ab99ab5e50cc6b3
Rocky.C <cloki0610@gmail.com>
2022-01-30 02:11:01 +0000

    Profile App

---
+++ profiles/urls.py
@@ -0,0 +1,9 @@
+""" URL's of the profiles application """
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.profile_display, name='profile'),
+    path('order_history/<order_number>', views.order_history,
+         name='order_history')
+]
